06211d1ae208d3172d1e53a1ddbd6caac66448dd
dofpo <z-salawat91@mail.ru>
2022-07-26 17:51:15 +0500

    initial OrderCardTest commit

--- src/test/java/ru/netology/test/OrderCardTest.java
+++ src/test/java/ru/netology/test/OrderCardTest.java
@@ -1,25 +1,24 @@
 package ru.netology.test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CallBackTest {
+public class OrderCaTest {
     private WebDriver driver;
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/win 10/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "driver/win 10/chromedriver.exe");
     }
 
     @BeforeEach
